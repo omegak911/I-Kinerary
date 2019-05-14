bb203a01a982ed8b91dad18cfd30fa8af3c5e311
Kin Chan <xkinchanx@gmail.com>
2019-05-14 10:52:35 -0700

    [feat] rendering google maps with 1 stop directions

---
+++ client/src/components/Map.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap, DirectionsRenderer } from 'react-google-maps';
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dummyCenter: {
+        lat: 34.0522,
+        lng: -118.2437
+      },
+      lineCoordinates: null,
+      directions: null
+    }
+  }
+
+  componentDidMount() {
+    this.handleMapLoad();
+  }
+
+  handleMapLoad = () => {
+    const directionsService = new google.maps.DirectionsService();
+
+    const calcAndDisplayRoute = (directionsService) => {
+      directionsService.route({
+        origin: 'Los Angeles',
+        destination: 'Santa Barbara',
+        travelMode: google.maps.TravelMode.DRIVING
+      }, (res, status) => {
+        if (status === 'OK') {
+          this.setState({ directions: res });
+        } else {
+          console.error('didnt work', res, status)
+        }
+      })
+    }
+    calcAndDisplayRoute(directionsService);
+  }
+
+
+  render() {
+    let { dummyCenter, directions } = this.state;
+    const GoogleMapContainer = withGoogleMap(props => (
+      <GoogleMap
+        defaultCenter={ dummyCenter }
+        defaultZoom={ 13 }
+      >
+        {directions && <DirectionsRenderer directions={directions}/>}
+      </GoogleMap>
+    ))
+
+    return (
+      <div>
+        <GoogleMapContainer 
+          containerElement={ <div style={{ height: '100vh', width: '100%' }}/>}
+          mapElement={ <div style={{ height: '100%' }}/>}
+        />
+      </div>
+    )
+  }
+}
+
+export default Map;
