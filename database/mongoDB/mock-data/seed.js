7726de5cd902c47209cc6512f002ac0a47f6e9ab
Kin Chan <xkinchanx@gmail.com>
2019-05-23 23:42:50 -0700

    [prog] script to alter data to proper format after it's been seeded

--- database/mongoDB/mock-data/seed.js
+++ database/mongoDB/mock-data/seed.js
@@ -1,35 +1,29 @@
-import { Trip, Stop } from '../models';
-import sequelize from '../index';
+import Route from '../collections';
 
-sequelize.sync({ force: true })
-  .then(() => {
-    //seed data
+Route
+  .find({})
+  .then(result => { 
+    result.forEach((element,i) => {
+      let waypointObj = JSON.parse(JSON.stringify(element.waypoints[0])); //it's stored as something that isn't actually an object
+      let waypoints = [];
+      let location = '';
+      for (let key in waypointObj) {
+        let letter = waypointObj[key];
+        if (letter === '|') {
+          waypoints.push({ location, stopover: true });
+          location = '';
+        } else {
+          location += letter;
+        }
+      }
+      Route.updateOne(
+        {
+          trip_id: element.trip_id
+        }, 
+        { 
+          waypoints: waypoints 
+        })
+        .then(() => console.log('success'))
+        .catch(err => { if (i === 0) console.log(err)});
+    })
   })
-
-
-
-
-/*
-
-
-{
-    "trip_id": 1, 
-    "origin": "Alhambra, CA", 
-    "destination": "Santa Barbara, CA", 
-    "waypoints": [
-    	{
-    		"location": "Los Angeles, CA",
-    		"stopover": true
-    	},
-    	{
-    		"location": "Santa Monica, CA",
-    		"stopover": true
-    	}
-    ],
-    "travelMode": "DRIVING"
-}
-
-
-
-
-*?
