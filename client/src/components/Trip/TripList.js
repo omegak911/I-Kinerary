a528f2fe6d69539ed1e10266458143a003799167
Kin Chan <xkinchanx@gmail.com>
2019-05-25 15:14:02 -0700

    [prog] rendering N items with button to render more

--- client/src/components/Trip/TripList.js
+++ client/src/components/Trip/TripList.js
@@ -7,7 +7,8 @@ class TripList extends Component {
   constructor(props){
     super(props);
     this.state = {
-      trips: []
+      trips: [],
+      numberOfTripsDisplayed: 5
     }
   }
 
@@ -17,27 +18,29 @@ class TripList extends Component {
     //but for MVP, we'll leave this here
     axios
       .get(`/api/auth?username=Omegak911`)
-      .then(({ data }) => {
-        this.setState({ trips: data.trips });
-      })
+      .then(({ data }) => this.setState({ trips: data.trips }))
       .catch(() => console.log('err'));
   }
 
+  increasedDisplayedTrips = () => {
+    this.setState({ numberOfTripsDisplayed: this.state.numberOfTripsDisplayed + 5})
+  }
+
   render() {
-    let trips = this.state.trips.map(trip => 
-      <TripListItem 
-        key={trip.id} 
-        trip={trip}
-        handleViewChange={this.props.handleViewChange}
-        />
-    )
+    let trips = this.state.trips.map((trip , i) => {
+      if (i < this.state.numberOfTripsDisplayed) {
+        return <TripListItem 
+          key={trip.id} 
+          trip={trip}
+          handleViewChange={this.props.handleViewChange}
+          />
+      }
+    })
+
     return (
       <div>TripList
         {trips}
-        {/* <div onClick={() => handleViewChange(1)}>Hardcoded Trip</div>
-        <div onClick={() => handleViewChange(1)}>Hardcoded Trip</div>
-        <div onClick={() => handleViewChange(1)}>Hardcoded Trip</div>
-        <div onClick={() => handleViewChange(1)}>Hardcoded Trip</div> */}
+        <button onClick={this.increasedDisplayedTrips}>Show more</button>
       </div>
     )
   }
