481e6ca172543708f4984709b4dfeb2575f3918f
Kin Chan <xkinchanx@gmail.com>
2019-05-27 13:35:57 -0700

    [style] border for list item + clamping description overflow

--- client/src/components/Trip/TripListItem.js
+++ client/src/components/Trip/TripListItem.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import ClampLines from 'react-clamp-lines';
 
 const TripListItem = ({ trip, handleViewChange }) => {
   let { id, title, description, start_date, end_date, stars } = trip;
+
   return (
-    <div onClick={() => handleViewChange(id)}>
-      <h5>{title}</h5>
-      <p>{description}</p>
+    <StyledTripListItem onClick={() => handleViewChange(id)}>
+      <h3>{title}</h3>
+      <ClampLines 
+        text={description}
+        id={id}
+        lines={1}
+        innerElement="p"
+      />
       <div>
         <span>start: {start_date}</span>
         <span>end: {end_date}</span>
         <span>stars: {stars}</span>
       </div>
-    </div>
+    </StyledTripListItem>
   )
 }
+
+const StyledTripListItem = styled.div`
+  border: 1px solid blue;
+  margin: 5px;
+  padding: 5px;
+`
 
 export default TripListItem;
