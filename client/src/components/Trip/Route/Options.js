9dd5edbfd8fc3af561903310d690eeb5002f30f3
Kin Chan <xkinchanx@gmail.com>
2019-06-14 00:05:11 -0700

    [prog] now showing save progress to client

--- client/src/components/Trip/Route/Options.js
+++ client/src/components/Trip/Route/Options.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+const count = 0;
+class Options extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      saveText: ''
+    }
+  }
 
-const Options = ({ route }) => {
-
-  const saveRoute = () => {
-    let { trip_id, waypoints, travelMode } = route;
+  saveRoute = async () => {
+    let { 
+      travelMode,
+      trip_id, 
+      waypoints
+    } = this.props.route;
     let options = {
       travelMode,
       waypoints
     }
-    axios
-      .patch('/api/route', { trip_id, options })
-      .then(data => console.log(data))
-      .catch(err => console.error(err));
+
+    try {
+      await this.changeSaveText('...saving');
+      axios
+        .patch('/api/route', { trip_id, options })
+        .then(() => this.changeSaveText('Saved!'))
+        .catch(() => this.changeSaveText('sorry, unable to save'));
+    } catch {
+      this.changeSaveText('sorry, unable to save');
+    }
   }
 
-  return (
-    <div>
-      optimize
-      <button type="button" onClick={saveRoute}>save</button>
-    </div>
-  )
+  changeSaveText = async (saveText) => {
+    try {
+      await this.setState({ saveText });
+    } catch {
+      await this.setState({ saveText: 'sorry, unable to save'});
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        optimize
+        <button type="button" onClick={this.saveRoute}>save</button>
+        {this.state.saveText}
+      </div>
+    )
+  }
 }
 
 export default Options;
