4632e6d9a4759fed0417c1f119fa6f3092e27a1f
Kin Chan <xkinchanx@gmail.com>
2019-06-15 15:48:45 -0700

    [cleanup] moving files for better organization

---
+++ client/config/example.config.js
@@ -0,0 +1,5 @@
+const GMAPS_URL = "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY";
+const API_KEY = "YOUR_API_KEY";
+const SOCKET_URL = 'http://localhost:3009';
+
+export { GMAPS_URL, API_KEY, SOCKET_URL };
--- socket-server/src/index.js
+++ socket-server/src/index.js
@@ -5,8 +5,20 @@ const PORT = 3009;
 const server = http.createServer();
 const io = SocketIO(server);
 
-io.on('connection', (socket) => {
+io.on('connection', (client) => {
   console.log('user is connected')
+
+  client.on('event', data => {
+    console.log(`client event data: ${data}`)
+  })
+
+  client.on('client.updateRoute', data => {
+    console.log(`client updateRoute: ${data}`)
+  })
+
+  client.on('disconnect', () => {
+    console.log(`client disconnected`)
+  })
 })
 
 server.listen(PORT, () => console.log('socket IO server is operational'));
