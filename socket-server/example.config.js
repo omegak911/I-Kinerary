d18e7933a53ec6e5e4c7bb268349bb16467209b3
Kin Chan <xkinchanx@gmail.com>
2019-06-16 00:22:37 -0700

    [prog] when client sends a comment, socket will make an API call to server to persist it before broadcasting

---
+++ socket-server/example.config.js
@@ -0,0 +1,3 @@
+const server_comment_URL = 'http:localhost:3005/route/comments';
+
+module.exports = server_comment_URL;
--- socket-server/src/clientEvents.js
+++ socket-server/src/clientEvents.js
@@ -1,15 +1,25 @@
+import axios from 'axios';
+
+import server_comment_URL from '../config';
+
 import { 
   serverUpdateRoute,
   serverSendMessage
 } from './serverEvents';
 
 const clientUpdateRoute = ({ io, room }, payload) => {
-  console.log('reached clientUpdateRoute');
   serverUpdateRoute(io, room, payload);
 }
 
 const clientSendMessage = ({ io, room }, payload) => {
-  serverSendMessage(io, room, payload);
+  if (payload.type === 'Comments') {
+    axios
+      .post(server_comment_URL, payload)
+      .then(() => serverSendMessage(io, room, payload))
+      .catch(err => console.error('clientSendMessage err: ', err))
+  } else  {
+    serverSendMessage(io, room, payload);
+  }
 }
 
 export default {
